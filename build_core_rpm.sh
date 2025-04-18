a017538001baa881693738b70fa3700aee3c0709
Xiangce Liu <xiangceliu@redhat.com>
2025-04-18 16:24:02 +0800

    WIP
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- build_core_rpm.sh
+++ build_core_rpm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 PYTHON=${1:-python}
 
-rm -rf BUILD BUILDROOT RPMS SRPMS
+rm -rf BUILD BUILDROOT RPMS SRPMS SPECS
 rm -rf insights_core.egg-info
-cp MANIFEST.in.core MANIFEST.in
+cp MANIFEST.in.pkg MANIFEST.in
 $PYTHON setup.py sdist
 rpmbuild -ba -D "_topdir $PWD" -D "_sourcedir $PWD/dist" insights-core.spec
-rm -rf dist BUILD BUILDROOT
+rm -rf dist BUILD BUILDROOT SPECS
 git checkout MANIFEST.in
