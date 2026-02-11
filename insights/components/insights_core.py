0f273b2fa1e818c6deaa65dc8a7f73244ca5307d
Xiangce Liu <xiangceliu@redhat.com>
2026-02-11 19:06:12 +0800

    feat: add CoreEgg/CoreRpm to identify current collector
    These two components can be used to skip Specs based on type of
    the current collector.
    This is a preparation for RHINENG-24031
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/components/insights_core.py
+++ insights/components/insights_core.py
@@ -14,18 +14,26 @@ from insights.core.plugins import component
 from insights.parsers.installed_rpms import InstalledRpms, InstalledRpm
 
 
+<<<<<<< HEAD
 def _identify_collector(rpms):
+=======
+def _is_RPM_collector(rpms):
+>>>>>>> cf802d30 (feat: add CoreEgg/CoreRpm to identify current collector)
     """
     Check the installed RPMs, it's RPM collector only when
     - insights-core is installed
     - insights-client is newer than specified minmum version
     """
+<<<<<<< HEAD
     # TODO: add rhc
+=======
+>>>>>>> cf802d30 (feat: add CoreEgg/CoreRpm to identify current collector)
     min_client = {
         'el9': InstalledRpm('insights-client-3.9.3-1.el9'),
         'el10': InstalledRpm('insights-client-3.10.3-1.el10'),
     }
 
+<<<<<<< HEAD
     if 'insights-client' in rpms:
         # Offically, insights-client.rpm must be installed
         cur_client = rpms.newest('insights-client')
@@ -40,6 +48,18 @@ def _identify_collector(rpms):
         return 'EGG'
     # Unknown
     return None
+=======
+    # insights-client must be installed
+    cur_client = rpms.newest('insights-client')
+    if 'insights-core' in rpms:
+        # insights-core.rpm is installed
+        for el, client in min_client.items():
+            if el in cur_client.release and cur_client >= client:
+                # insights-client that uses RPM is installed
+                return True
+    # Egg is used by default
+    return False
+>>>>>>> cf802d30 (feat: add CoreEgg/CoreRpm to identify current collector)
 
 
 @component(InstalledRpms)
@@ -49,12 +69,21 @@ class CoreEgg(object):
     is used for this collection.
 
     Raises:
+<<<<<<< HEAD
         SkipComponent: When RPM/Unknown collector is used.
     """
 
     def __init__(self, rpms):
         if _identify_collector(rpms) != 'EGG':
             raise SkipComponent('Egg is not used.')
+=======
+        SkipComponent: When RPM collector is used.
+    """
+
+    def __init__(self, rpms):
+        if _is_RPM_collector(rpms):
+            raise SkipComponent('core RPM is used.')
+>>>>>>> cf802d30 (feat: add CoreEgg/CoreRpm to identify current collector)
 
 
 @component(InstalledRpms)
@@ -64,9 +93,18 @@ class CoreRpm(object):
     is used for this collection.
 
     Raises:
+<<<<<<< HEAD
         SkipComponent: When Egg/Unknown collector is used.
     """
 
     def __init__(self, rpms):
         if _identify_collector(rpms) != 'RPM':
             raise SkipComponent('RPM is not used.')
+=======
+        SkipComponent: When Egg collector is used.
+    """
+
+    def __init__(self, rpms):
+        if _is_RPM_collector(rpms) is False:
+            raise SkipComponent('core Egg is used.')
+>>>>>>> cf802d30 (feat: add CoreEgg/CoreRpm to identify current collector)
--- insights/tests/components/test_insights_core.py
+++ insights/tests/components/test_insights_core.py
@@ -6,11 +6,16 @@ from insights.parsers.installed_rpms import InstalledRpms
 from insights.tests import context_wrap
 
 
+<<<<<<< HEAD
 RPMS_CLIENT_CORE = """
+=======
+RPMS_CORE = """
+>>>>>>> cf802d30 (feat: add CoreEgg/CoreRpm to identify current collector)
 {"name":"insights-core","epoch":"(none)","version":"3.7.2.2","release":"2.el9","arch":"x86_64","installtime":"Mon Mar 26 16:32:08 2018","buildtime":"1457608025","vendor":"Red Hat, Inc.","buildhost":"x86-024.build.eng.bos.redhat.com","sigpgp":"RSA/SHA256, Fri Mar 18 05:15:10 2016, Key ID 199e2f91fd431d51"}
 {"name":"insights-client","epoch":"(none)","version":"3.9.3","release":"1.el9","arch":"x86_64","installtime":"Mon Mar 26 16:32:08 2018","buildtime":"1457608025","vendor":"Red Hat, Inc.","buildhost":"x86-024.build.eng.bos.redhat.com","sigpgp":"RSA/SHA256, Fri Mar 18 05:15:10 2016, Key ID 199e2f91fd431d51"}
 """
 
+<<<<<<< HEAD
 RPMS_NO_CLIENT_CORE = """
 {"name":"insights-core","epoch":"(none)","version":"3.7.2.2","release":"2.el9","arch":"x86_64","installtime":"Mon Mar 26 16:32:08 2018","buildtime":"1457608025","vendor":"Red Hat, Inc.","buildhost":"x86-024.build.eng.bos.redhat.com","sigpgp":"RSA/SHA256, Fri Mar 18 05:15:10 2016, Key ID 199e2f91fd431d51"}
 """
@@ -20,31 +25,52 @@ RPMS_CLIENT_NO_CORE = """
 """
 
 RPMS_OLD_CLIENT_CORE = """
+=======
+RPMS_NO_CORE = """
+{"name":"insights-client","epoch":"(none)","version":"3.9.3","release":"1.el9","arch":"x86_64","installtime":"Mon Mar 26 16:32:08 2018","buildtime":"1457608025","vendor":"Red Hat, Inc.","buildhost":"x86-024.build.eng.bos.redhat.com","sigpgp":"RSA/SHA256, Fri Mar 18 05:15:10 2016, Key ID 199e2f91fd431d51"}
+"""
+
+RPMS_CORE_CLIENT_NG = """
+>>>>>>> cf802d30 (feat: add CoreEgg/CoreRpm to identify current collector)
 {"name":"insights-core","epoch":"(none)","version":"3.7.2.2","release":"2.el9_2","arch":"x86_64","installtime":"Mon Mar 26 16:32:08 2018","buildtime":"1457608025","vendor":"Red Hat, Inc.","buildhost":"x86-024.build.eng.bos.redhat.com","sigpgp":"RSA/SHA256, Fri Mar 18 05:15:10 2016, Key ID 199e2f91fd431d51"}
 {"name":"insights-client","epoch":"(none)","version":"3.9.0","release":"1.el9","arch":"x86_64","installtime":"Mon Mar 26 16:32:08 2018","buildtime":"1457608025","vendor":"Red Hat, Inc.","buildhost":"x86-024.build.eng.bos.redhat.com","sigpgp":"RSA/SHA256, Fri Mar 18 05:15:10 2016, Key ID 199e2f91fd431d51"}
 """
 
 
+<<<<<<< HEAD
 def test_core_egg():
     rpms = InstalledRpms(context_wrap(RPMS_CLIENT_NO_CORE))
+=======
+def test_egg_and_rpm():
+    rpms = InstalledRpms(context_wrap(RPMS_NO_CORE))
+>>>>>>> cf802d30 (feat: add CoreEgg/CoreRpm to identify current collector)
     egg = CoreEgg(rpms)
     assert egg is not None
     with pytest.raises(SkipComponent):
         CoreRpm(rpms)
 
+<<<<<<< HEAD
     rpms = InstalledRpms(context_wrap(RPMS_OLD_CLIENT_CORE))
+=======
+    rpms = InstalledRpms(context_wrap(RPMS_CORE_CLIENT_NG))
+>>>>>>> cf802d30 (feat: add CoreEgg/CoreRpm to identify current collector)
     egg = CoreEgg(rpms)
     assert egg is not None
     with pytest.raises(SkipComponent):
         CoreRpm(rpms)
 
+<<<<<<< HEAD
 
 def test_core_rpm():
     rpms = InstalledRpms(context_wrap(RPMS_CLIENT_CORE))
+=======
+    rpms = InstalledRpms(context_wrap(RPMS_CORE))
+>>>>>>> cf802d30 (feat: add CoreEgg/CoreRpm to identify current collector)
     with pytest.raises(SkipComponent):
         CoreEgg(rpms)
     rpm = CoreRpm(rpms)
     assert rpm is not None
+<<<<<<< HEAD
 
 
 def test_core_unknown():
@@ -54,3 +80,5 @@ def test_core_unknown():
         CoreRpm(rpms)
     with pytest.raises(SkipComponent):
         CoreEgg(rpms)
+=======
+>>>>>>> cf802d30 (feat: add CoreEgg/CoreRpm to identify current collector)
